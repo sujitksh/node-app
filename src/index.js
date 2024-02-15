4891645433a22feec0559ca3e78eab981017ce2f
Sujit Kumar <sujit.k@tavant.com>
2024-02-15 16:52:43 +0530

    second commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+ const app = express();
+ 
+ const port = process.env.PORT || 4000;
+
+app.get("/",(req,res)=>{
+  res.send("hello world");
+})
+ app.listen(port,()=>{
+   console.log(`Running port ${port}`)
+ })

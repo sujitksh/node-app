e24e6043578d16f70a8748e235bba8c34ee8d45d
Sujit Kumar <sujit.k@tavant.com>
2024-02-15 17:04:46 +0530

    changes in index file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import express from "express";
  const port = process.env.PORT || 4000;
 
 app.get("/",(req,res)=>{
-  res.send("hello world");
+  res.send("hello bhai");
 })
  app.listen(port,()=>{
    console.log(`Running port ${port}`)
